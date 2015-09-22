e166223ebe6420548a4bbd5bea06dd3eee7437c8
Ariel Keselman <skariel@gmail.com>
2015-09-22 16:59:15 +0300

    More async for requests

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,7 +1,5 @@
 package controllers;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import jdk.nashorn.internal.parser.JSONParser;
 import play.libs.F;
 import play.libs.Json;
 import play.libs.ws.WS;
@@ -10,7 +8,6 @@ import play.libs.ws.WSResponse;
 import play.mvc.*;
 
 import play.twirl.api.Html;
-import scala.util.parsing.json.JSONObject;
 import views.html.*;
 
 import java.util.concurrent.TimeUnit;
@@ -28,57 +25,59 @@ public class Application extends Controller {
         }
         return redirect(github_access.uri_logged_in);
     }
-    public Result just_received_code() {
+    public F.Promise<Result> just_received_code() {
         String code = request().getQueryString("code");
         String state = request().getQueryString("state");
         if (state.equals(session().get("state"))) {
             session().put("github_code", code);
-            WSResponse res = github_access.get_github_access_token(state, code).execute().get(60, TimeUnit.SECONDS);
-            String body = res.getBody();
-            String[] splitted = body.split("\\&");
-            if ((splitted.length!=3) || (!splitted[0].contains("=")) || (!splitted[1].contains("=")) || (!splitted[2].contains("="))) {
-                return unauthorized();
-            }
-            String token = splitted[0].split("\\=")[1];
-            String token_type = splitted[2].split("\\=")[1];
-            String scope = splitted[1].split("\\=")[1];
-            session().put("token", token);
-            session().put("token_type", token_type);
-            session().put("scope", scope);
-            return redirect(github_access.uri_logged_in);
+            return F.Promise.promise(() -> {
+                WSResponse res = github_access.get_github_access_token(state, code).execute().get(60, TimeUnit.SECONDS);
 
+                String body = res.getBody();
+                String[] splitted = body.split("\\&");
+                if ((splitted.length != 3) || (!splitted[0].contains("=")) || (!splitted[1].contains("=")) || (!splitted[2].contains("="))) {
+                    return unauthorized();
+                }
+                String token = splitted[0].split("\\=")[1];
+                String token_type = splitted[2].split("\\=")[1];
+                String scope = splitted[1].split("\\=")[1];
+                session().put("token", token);
+                session().put("token_type", token_type);
+                session().put("scope", scope);
+                return redirect(github_access.uri_logged_in);
+
+            });
         }
-        return unauthorized();
+        return F.Promise.promise(() -> unauthorized());
     }
 
-    public Result just_logged_in() {
+    public F.Promise<Result> just_logged_in() {
+        return F.Promise.promise(()-> {
+            WSResponse res_rep;
+            WSResponse res_user;
+            try {
+                WSRequest req_rep = WS.url("https://api.github.com/user/repos")
+                        .setHeader("Authorization", "token " + session().get("token"))
+                        .setHeader("Accept", "application/vnd.github.v3 + json")
+                        .setMethod("GET");
+                F.Promise<WSResponse> pres_rep = req_rep.execute();
+                WSRequest req_user = WS.url("https://api.github.com/user")
+                        .setHeader("Authorization", "token " + session().get("token"))
+                        .setHeader("Accept", "application/vnd.github.v3 + json")
+                        .setMethod("GET");
+                F.Promise<WSResponse> pres_user = req_user.execute();
+                res_rep = pres_rep.get(60, TimeUnit.SECONDS);
+                res_user = pres_user.get(60, TimeUnit.SECONDS);
+                String avatar_url = Json.parse(res_user.getBody())
+                        .get("avatar_url")
+                        .asText();
+                session().put("avatar_url", avatar_url);
 
-        //return ok(main.render("title!", Html.apply("Your repos: "+session().get("token"))));//res.getBody()+" status: "+res.getStatusText())));
-        WSResponse res_rep;
-        WSResponse res_user;
-        try {
-            WSRequest req_rep = WS.url("https://api.github.com/user/repos")
-                    .setHeader("Authorization", "token " + session().get("token"))
-                    .setHeader("Accept", "application/vnd.github.v3 + json")
-                    .setMethod("GET");
-            F.Promise<WSResponse> pres_rep = req_rep.execute();
-            res_rep = pres_rep.get(60, TimeUnit.SECONDS);
-            WSRequest req_user = WS.url("https://api.github.com/user")
-                    .setHeader("Authorization", "token " + session().get("token"))
-                    .setHeader("Accept", "application/vnd.github.v3 + json")
-                    .setMethod("GET");
-            F.Promise<WSResponse> pres_user = req_user.execute();
-            res_user = pres_user.get(60, TimeUnit.SECONDS);
-            String avatar_url = Json.parse(res_user.getBody())
-                    .get("avatar_url")
-                     .asText();
-            session().put("avatar_url", avatar_url);
-
-        }
-        catch (Exception e) {
-            return ok(main.render("title!", Html.apply(e.toString())));
-        }
-        return ok(main.render("title!", Html.apply("<img src=\""+session("avatar_url")+"\" alt=\"avatar\" style=\"width:304px;height:304px;\">"+"   Your repos: " + res_rep.getBody())));
+            } catch (Exception e) {
+                return ok(main.render("title!", Html.apply(e.toString())));
+            }
+            return ok(main.render("title!", Html.apply("<img src=\"" + session("avatar_url") + "\" alt=\"avatar\" style=\"width:304px;height:304px;\">" + "   Your repos: " + res_rep.getBody())));
+        });
     }
 
     public Result logout() {
