faa056a7a58b9dd046551c6e5ad83e6aabf10b7e
Ariel Keselman <skariel@gmail.com>
2015-10-02 23:31:53 +0300

    stuff

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,26 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table gmail_last_date_read (
+  id                        varchar(255) not null,
+  lastdate                  timestamp,
+  constraint pk_gmail_last_date_read primary key (id))
+;
+
+create sequence gmail_last_date_read_seq;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists gmail_last_date_read;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists gmail_last_date_read_seq;
+
