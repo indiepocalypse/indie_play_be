93e4f647a3f81e3faab2b5e7ce2680acf73d4498
Ariel Keselman <skariel@gmail.com>
2015-10-07 11:59:00 +0300

    evolved evolutions

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -19,9 +19,24 @@ create table repo_model (
   constraint pk_repo_model primary key (repo_name))
 ;
 
+create table user_model (
+  user_name                 varchar(255) not null,
+  user_blog_url             varchar(255),
+  github_html_url           varchar(255),
+  email                     varchar(255),
+  avatar_url                varchar(255),
+  public_repos              integer,
+  github_repos_url          varchar(255),
+  followers                 integer,
+  following                 integer,
+  constraint pk_user_model primary key (user_name))
+;
+
 create sequence gmail_last_date_read_seq;
 
 create sequence repo_model_seq;
+
+create sequence user_model_seq;
 
 
 
@@ -34,9 +49,13 @@ drop table if exists gmail_last_date_read;
 
 drop table if exists repo_model;
 
+drop table if exists user_model;
+
 SET REFERENTIAL_INTEGRITY TRUE;
 
 drop sequence if exists gmail_last_date_read_seq;
 
 drop sequence if exists repo_model_seq;
+
+drop sequence if exists user_model_seq;
 
