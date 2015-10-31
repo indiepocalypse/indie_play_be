ae0ad98c1ac96abb34e0466bf90edfbb1b63727a
Ariel Keselman <skariel@gmail.com>
2015-10-31 17:59:46 +0200

    starting commandnds refactoring

--- app/commands/command_hi.java
+++ app/commands/command_hi.java
@@ -5,7 +5,7 @@ import models_github.model_command;
 /**
  * Created by skariel on 31/10/15.
  */
-public class command_close implements interface_command {
+public class command_hi implements interface_command {
     // TODO: implement!
     @Override
     public boolean is_recognized(model_command command) {
