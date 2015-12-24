dd1756cc9febff95f3701515054bf74916c36800
Ariel Keselman <skariel@gmail.com>
2015-12-24 12:24:34 +0200

    debugging

--- app/Global.java
+++ app/Global.java
@@ -6,11 +6,22 @@ import sync.sync_github_repos;
 import sync.sync_github_users;
 import sync.sync_gmail;
 
+import java.io.File;
+
 public class Global extends GlobalSettings {
 
     @Override
     public void onStart(Application app) {
         super.onStart(app);
+
+        // sjust debugging somethig XXXXXXXXXXXX
+        File directory = new File("/");
+        File[] fList = directory.listFiles();
+        for (File file : fList){
+            Logger.info(" ------ "+file.getPath()+" ---------- " + file.getName());
+        }
+
+
         Logger.info("global start called");
         if (store_conf.get_debug_should_check_mails()) {
             Logger.info("starting gmail sync");
